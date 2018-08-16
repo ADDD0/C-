69bd9c3aaa2685daecad7d6f6aeaedf3a8a8c0fc
ADDD0 <448417505@qq.com>
2018-08-16 14:08:27 +0800

    14:08 2018/8/16

---
+++ C++面向对象程序设计（第2版） 谭浩强/第1章 C++的初步知识/EE1.1.cpp
@@ -0,0 +1,7 @@
+//输出一行字符:"This is a C++ Program."
+#include<iostream>                    //用cout输出时需要用此头文件
+using namespace std;                  //使用命名空间std
+int main()
+  {cout<<"This is a C++ program.\n";  //用C++的方法输出一行
+   return 0;
+  }
---
+++ C++面向对象程序设计（第2版） 谭浩强/第1章 C++的初步知识/EE1.3.cpp
@@ -0,0 +1,18 @@
+//从键盘输入两个数a和b,求两数中的大者
+#include<iostream>
+using namespace std;
+int main()
+  {int max(int x,int y);  //对max函数作声明
+   int a,b,c;
+   cin>>a>>b;
+   c=max(a,b);            //调用max函数
+   cout<<"max="<<c<<endl;
+   return 0;
+  }
+
+int max(int x,int y)      //定义max函数
+  {int z;
+   if(x>y) z=x;
+   else z=y;
+   return(z);
+  }
