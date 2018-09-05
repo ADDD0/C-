5ff00451e42ea4eef5412d50b319d441ec05afd8
ADDD0 <448417505@qq.com>
2018-09-05 21:58:32 +0800

    21:58 2018/9/5

---
+++ C++面向对象程序设计（第2版） 谭浩强/第2章 类和对象的特性/2.5 类和对象的简单应用举例/EE2.2.a.cpp
@@ -0,0 +1,23 @@
+//用上例中的Time类，定义多个类对象，分别输入和输出各对象中的时间（时：分：秒）。
+#include<iostream>
+using namespace std;
+class Time        //声明Time类
+  {public:
+   int hour;
+   int minute;
+   int sec;
+  };
+
+int main()
+  {Time t1;       //定义对象t1
+   cin>>t1.hour;  //向t1的数据成员输入数据
+   cin>>t1.minute;
+   cin>>t1.sec;
+   cout<<t1.hour<<":"<<t1.minute<<":"<<t1.sec<<endl;  //输出t1中数据成员的值
+   Time t2;       //定义对象t2
+   cin>>t2.hour;  //向t2的数据成员输入数据
+   cin>>t2.minute;
+   cin>>t2.sec;
+   cout<<t2.hour<<":"<<t2.minute<<":"<<t2.sec<<endl;  //输出t2中数据成员的值
+   return 0;
+  }
---
+++ C++面向对象程序设计（第2版） 谭浩强/第2章 类和对象的特性/2.5 类和对象的简单应用举例/EE2.2.c.cpp
@@ -0,0 +1,31 @@
+//用上例中的Time类，定义多个类对象，分别输入和输出各对象中的时间（时：分：秒）。
+#include<iostream>
+using namespace std;
+class Time
+  {public:
+   int hour;
+   int minute;
+   int sec;
+  };
+
+int main()
+  {void set_time(Time&,int hour=0,int minute=0,int sec=0);  //函数声明,指定了默认参数
+   void show_time(Time&);  //函数声明
+   Time t1;
+   set_time(t1,12,23,34);  //通过实参传递时分秒的值
+   show_time(t1);
+   Time t2;
+   set_time(t2);           //使用默认的时分秒的值
+   show_time(t2);
+   return 0;
+  }
+
+void set_time(Time& t,int hour,int minute,int sec)          //定义函数时不必再指定了默认参数
+  {t.hour=hour;
+   t.minute=minute;
+   t.sec=sec;
+  }
+
+void show_time(Time& t)
+  {cout<<t.hour<<":"<<t.minute<<":"<<t.sec<<endl;
+  }
