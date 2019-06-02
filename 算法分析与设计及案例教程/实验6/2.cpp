648acd64b1a687dfdd0b30d6b3be7ff30fcd9881
ADDD0 <448417505@qq.com>
2019-06-02 08:22:02 +0800

    8:21 2019/6/2

---
+++ 算法分析与设计及案例教程/实验6/2.cpp
@@ -0,0 +1,72 @@
+/**
+ * 使用动态规划解决硬币收集问题：机器人从左上角到右下角对一个格子里面的硬币
+ * 进行收集，每个格子最多有一枚硬币，机器人只能往下或往右走，某些格子是不允许
+ * 机器人通过的，求机器人最终收集到的硬币数目以及最优的路径数目。
+ */
+
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+const int MAXN = 1000;
+int arr[MAXN][MAXN];
+int f[MAXN][MAXN];
+int arr2[MAXN][MAXN];
+int main()
+{
+	int n, m;
+	cout << "请输入格木板的行数（n）:";
+	cin >> n;
+	cout << "请输入格木板的列数（m）:";
+	cin >> m;
+	cout << "请输入格木板上硬币放置情况（有硬币为1，没有为0,中间以空格分开）：" << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i <= n; i++)
+	{
+		f[i][0] = 0;
+		f[0][i] = 0;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			f[i][j] = arr[i][j] + max(f[i - 1][j], f[i][j - 1]);
+		}
+	}
+	//通过回溯得到行走路劲
+	int i = n;
+	int j = m;
+	memset(arr2, 0, sizeof(arr2));
+	arr2[1][1] = 1;
+	arr2[n][m] = 1;
+	while (i >= 1 && j >= 1)
+	{
+		if (f[i - 1][j] > f[i][j - 1])
+		{
+			arr2[i - 1][j] = 1;
+			i--;
+		}
+		else
+		{
+			arr2[i][j - 1] = 1;
+			j--;
+		}
+	}
+	cout << "最多可以收集 " << f[n][m] << " 枚硬币" << endl;
+	cout << "硬币的收集路线为:" << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cout << arr2[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ 算法分析与设计及案例教程/实验7/1.cpp
@@ -0,0 +1,39 @@
+/**
+ * 时间安排问题。有n项工作，每项工作分别在Si开始，Ti结束。
+ * 例如S={1,2,4,6,8}，T={3,5,7,8,10}。对每项工作，你都可以选择与否，若选择参加，
+ * 则必须至始至终参加全程参与，且参与工作的时间段不能有重叠。
+ */
+#include<iostream>
+#include<algorithm>
+#include<cstdlib>
+using namespace std;
+#define Max_N 100000
+//int start[Max_N];
+//int end[Max_N];
+int n;
+pair<int,int> p[Max_N];
+int main()
+{
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		int start,end;
+	   cin>>start>>end;
+       p[i]=make_pair(end,start);	
+	}                               //输入数据，并组成pair对保存在数组中 
+	sort(p,p+n);                     //按照pair中第一个关键词进行升序排序 
+	
+	int last=0,ans=0;
+	cout<<"选择的工作的起始时间有:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		if(p[i].second>last)             //现在寻找的下一个工作的开始时间大于上一次工作的结束时间，则选择该次工作 
+		  {
+		    last=p[i].first;
+		    ans++;
+		    cout<<p[i].second<<"--"<<p[i].first<<endl;
+		  }
+	}
+	cout<<"共选择了 "<<ans<<" 个工作"<<endl;
+	return 0;
+}
