c4c810d3c18c1adab11dae478f1dc09d29be4724
ADDD0 <448417505@qq.com>
2019-12-08 20:07:03 +0800

    20:06 2019/12/8

--- 操作系统/实验4/4.cpp
+++ 操作系统/实验4/4.cpp
@@ -21,17 +21,28 @@ class pageTable {
             existFlag = 1-existFlag;
         }
 
+        int getPageNumber() {
+            return pageNumber;
+        }
+
+        int getBlockNumberOnMemory() {
+            return blockNumberOnMemory;
+        }
+
         int getBlockNumberOnDisk() {
             return blockNumberOnDisk;
         }
 
-        void changeBlockNumberOnMemory(pageTable *pt) {
-            this->blockNumberOnMemory = pt->blockNumberOnMemory;
-            pt->changeBlockNumberOnMemory(-1);
+        void setBlockNumberOnMemory(int bm) {
+            blockNumberOnMemory = bm;
         }
 
-        void changeBlockNumberOnMemory(int num) {
-            blockNumberOnMemory = -1;
+        void swapBlockNumberOnMemory(pageTable &pt) {
+            int temp;
+
+            temp = pt.blockNumberOnMemory;
+            pt.setBlockNumberOnMemory(blockNumberOnMemory);
+            setBlockNumberOnMemory(temp);
         }
 
         void print() {
@@ -44,18 +55,19 @@ class pageTable {
 };
 
 int blockSize = 1024;
-int FIFO[3] = {0, 1, 2};
 pageTable *pt = new pageTable[8];
+pageTable *FIFO = new pageTable[3];
 
-void initPageTable() {
-    pt[0] = pageTable(0, true, 0, 0, 2);
-    pt[1] = pageTable(1, true, 1, 0, 3);
-    pt[2] = pageTable(2, true, 2, 0, 5);
-    pt[3] = pageTable(3, false, -1, 0, 7);
-    pt[4] = pageTable(4, false, -1, 0, 11);
-    pt[5] = pageTable(5, false, -1, 0, 13);
-    pt[6] = pageTable(6, false, -1, 0, 17);
-    pt[7] = pageTable(7, false, -1, 0, 19);
+void init() {
+    FIFO[0] = pt[0] = pageTable(0, true, 0, false, 2);
+    FIFO[1] = pt[1] = pageTable(1, true, 1, false, 3);
+    FIFO[2] = pt[2] = pageTable(2, true, 2, false, 5);
+    pt[3] = pageTable(3, false, -1, false, 7);
+    pt[4] = pageTable(4, false, -1, false, 11);
+    pt[5] = pageTable(5, false, -1, false, 13);
+    pt[6] = pageTable(6, false, -1, false, 17);
+    pt[7] = pageTable(7, false, -1, false, 19);
+
 }
 
 void printPageTable() {
@@ -69,19 +81,21 @@ void printPageTable() {
 }
 
 void pageFaultHandling(int addPage) {
-    static int removePage = 0;
+    static int removePage=0;
 
-    cout << "移出页面" << FIFO[removePage] << "并移入页面" << addPage << endl;
-    pt[FIFO[removePage]].changeExistFlag();
+    cout << "移出页面" << FIFO[removePage].getPageNumber() << "并移入页面"
+    << pt[addPage].getPageNumber() << endl;
+    FIFO[removePage].changeExistFlag();
     pt[addPage].changeExistFlag();
-    pt[addPage].changeBlockNumberOnMemory(pt+FIFO[removePage]);
-    FIFO[removePage] = addPage;
+    FIFO[removePage].swapBlockNumberOnMemory(pt[addPage]);
+    pt[FIFO[removePage].getPageNumber()] = FIFO[removePage];
+    FIFO[removePage] = pt[addPage];
 
     removePage = (removePage+1) % 3;
 }
 
 int main() {
-    initPageTable();
+    init();
 
     int addr=0;
     int pageNumber, pageAddress;
@@ -91,18 +105,21 @@ int main() {
         cin >> addr;
         if (!addr)
             break;
-        else if (addr < 1 || addr > 8191) {
+        else if (addr < 1 || addr > blockSize*8-1) {
             cout << "地址非法,请重新输入!!" << endl;
             continue;
         } else {
             pageNumber = addr / blockSize;
             pageAddress = addr % blockSize;
-            cout << "该逻辑地址对应的页号和页内地址分别为" << pageNumber << " " << pageAddress << endl;
+            cout << "该逻辑地址对应的页号和页内地址分别为" << pageNumber << " "
+            << pageAddress << endl;
         }
         for (int i=0; i < 3; ++i)
-            if (pageNumber == FIFO[i]) {
+            if (pageNumber == FIFO[i].getPageNumber()) {
                 cout << "第" << pageNumber << "页在内存中" << endl;
-                cout << "该逻辑地址对应物理地址为" << pt[pageNumber].getBlockNumberOnDisk() << "*" << blockSize << "+" << pageAddress << "=" << pt[pageNumber].getBlockNumberOnDisk()*blockSize + pageAddress << endl;
+                cout << "该逻辑地址对应物理地址为" << FIFO[i].getBlockNumberOnDisk()
+                << "*" << blockSize << "+" << pageAddress << "=" <<
+                FIFO[i].getBlockNumberOnDisk()*blockSize + pageAddress << endl;
                 break;
             } else if (i == 2) {
                 cout << "第" << pageNumber << "页不在内存中" << endl;
